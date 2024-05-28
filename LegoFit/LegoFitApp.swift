f94923477efd73e32ee5736c0f40c282767c4283
Denis Denisov <denis.denisov8701@gmail.com>
2024-05-28 19:08:05 +0200

    Initial Commit

---
+++ LegoFit/LegoFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoFitApp.swift
+//  LegoFit
+//
+//  Created by Denis Denisov on 28/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LegoFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
