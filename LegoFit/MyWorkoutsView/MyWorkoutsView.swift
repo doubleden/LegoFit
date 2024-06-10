0a4dc346737422de57454fb2a8e5854dcb4ab5a2
Denis Denisov <denis.denisov8701@gmail.com>
2024-06-10 20:19:02 +0200

    delete nav button in MyWorkoutsView

--- LegoFit/MyWorkoutsView/MyWorkoutsView.swift
+++ LegoFit/MyWorkoutsView/MyWorkoutsView.swift
@@ -11,26 +11,20 @@ import SwiftData
 struct MyWorkoutsView: View {
     @Query var workouts: [Workout]
     @Environment(\.modelContext) var modelContext
-    
+
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(workouts) { workout in
-                    NavigationLink(workout.name, destination: MyWorkoutsDetailsView(workout: workout))
+            ZStack {
+                List {
+                    ForEach(workouts) { workout in
+                        NavigationLink(workout.name, destination: MyWorkoutsDetailsView(workout: workout))
+                    }
+                    .onDelete(perform: { indexSet in
+                        deleteWorkout(indexSet)
+                    })
                 }
-                .onDelete(perform: { indexSet in
-                    deleteWorkout(indexSet)
-                })
             }
             .navigationTitle("My Workouts")
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    NavigationLink(destination: CreateWorkoutView()) {
-                        Image(systemName: "plus")
-                            .imageScale(.large)
-                    }
-                }
-            }
         }
     }
     
