53da2ed82e05de080badad698f3a437b407867d6
Denis Denisov <denis.denisov8701@gmail.com>
2024-06-14 22:21:46 +0200

    changed model

--- LegoFit/Models/WorkoutDTO.swift
+++ LegoFit/Models/WorkoutDTO.swift
@@ -25,9 +25,9 @@ struct ExerciseDTO: Decodable, Identifiable {
     let description: String
     let image: String
     
-    var set: Int?
-    var rep: Int?
-    var weight: Int?
+    let set: Int?
+    let rep: Int?
+    let weight: Int?
     
     static func getExercise() -> ExerciseDTO {
         ExerciseDTO(id: UUID(), queue: 1, category: "legs", name: "Жим ног", description: "Упражнение для прокачки ног, которое выполняется в специальном тренажёре, горизонтальном или вертикальном (наклонном)", image: "http://127.0.0.1:8080/images/leg_press.png", set: 0, rep: 0, weight: 0)
