3bbc524db83adfb3854b4890b0602594bb2b1b10
Denis Denisov <denis.denisov8701@gmail.com>
2024-06-16 00:13:57 +0200

    add appIcon

---
+++ LegoFit/View/GradientBackground.swift
@@ -0,0 +1,18 @@
+//
+//  GradientBackground.swift
+//  LegoFit
+//
+//  Created by Denis Denisov on 15/6/24.
+//
+
+import SwiftUI
+
+struct GradientBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradientBackground()
+}
---
+++ LegoFit/View/ParameterTextFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  ParameterTextFieldView.swift
+//  LegoFit
+//
+//  Created by Denis Denisov on 15/6/24.
+//
+
+import SwiftUI
+
+struct ParameterTextFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParameterTextFieldView()
+}
