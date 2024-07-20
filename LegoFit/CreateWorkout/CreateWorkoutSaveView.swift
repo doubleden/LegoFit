14a5bb11ee1d7f8aa4d238bc02436da80361db91
Denis Denisov <denis.denisov8701@gmail.com>
2024-07-20 16:21:14 +0200

    extract subviews

--- LegoFit/CreateWorkout/CreateWorkoutSaveView.swift
+++ LegoFit/CreateWorkout/CreateWorkoutSaveView.swift
@@ -16,30 +16,18 @@ struct CreateWorkoutSaveView: View {
     var body: some View {
         NavigationStack {
             VStack(alignment: .center, spacing: 25) {
-                TextField("Название тренировки", text: $createWorkoutVM.workout.name)
-                    .padding()
-                    .frame(width: 300, height: 50)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.main)
-                    )
+                InputNameTF(input: $createWorkoutVM.workout.name)
                 
-                Button(action: {
+                SaveButton(isDisabled: !createWorkoutVM.isWorkoutNameValid()) {
                     createWorkoutVM.saveWorkout(
                         modelContext: modelContext
                     )
                     createWorkoutVM.cancelCreateWorkout(
                         modelContext: modelContext
                     )
-                    
                     dismiss()
-                }, label: {
-                    Text("Сохранить тренировку")
-                        .tint(.white)
-                })
-                .font(.title2)
-                .buttonStyle(CustomButtonStyle(isDisabled: !createWorkoutVM.isWorkoutNameValid()))
-                .disabled(!createWorkoutVM.isWorkoutNameValid())
+                }
+                
                 Spacer()
             }
             .padding(.top, 40)
@@ -66,6 +54,35 @@ private struct CustomButtonStyle: ButtonStyle {
             .clipShape(.rect(cornerRadius: 5))
             .scaleEffect(configuration.isPressed ? 0.9 : 1)
             .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+struct InputNameTF: View {
+    @Binding var input: String
+    
+    var body: some View {
+        TextField("Название тренировки", text: $input)
+            .padding()
+            .frame(width: 300, height: 50)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.main)
+            )
+    }
+}
+
+struct SaveButton: View {
+    let isDisabled: Bool
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action, label: {
+            Text("Сохранить тренировку")
+                .tint(.white)
+        })
+        .font(.title2)
+        .buttonStyle(CustomButtonStyle(isDisabled: isDisabled))
+        .disabled(isDisabled)
     }
 }
 
