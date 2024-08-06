0d7d18f2422bfe7f15cbc420793abaf667a0fd35
Denis Denisov <denis.denisov8701@gmail.com>
2024-08-06 12:54:12 +0200

    changed ExerciseImageView

--- LegoFit/Views/ExerciseImageView.swift
+++ LegoFit/Views/ExerciseImageView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 import Kingfisher
 
-//TODO: Сделать картинку по умолчанию если не загружается
-//TODO: И Сделать ProgressView() пока картинка загружается
 struct ExerciseImageView: View {
     let imageUrl: String
     
     var body: some View {
         KFImage(URL(string: imageUrl))
+            .placeholder {
+                Image(systemName: "questionmark")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 30)
+            }
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(height: 200)
-            .clipShape(.rect(cornerRadius: 20))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
     }
 }
 
