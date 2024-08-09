1fe94a7eb9dc402d51ab2769a05f993f07b9d659
Denis Denisov <denis.denisov8701@gmail.com>
2024-08-09 15:33:15 +0200

    prepare ActiveWorkoutView and VM

--- LegoFit/ActiveWorkout/ActiveWorkoutView.swift
+++ LegoFit/ActiveWorkout/ActiveWorkoutView.swift
@@ -1,58 +1,23 @@
-////
-////  ActiveWorkoutView.swift
-////  LegoFit
-////
-////  Created by Denis Denisov on 27/6/24.
-////
 //
-//import SwiftUI
+//  ActiveWorkoutView.swift
+//  LegoFit
 //
-//struct ActiveWorkoutView: View {
-//    @Bindable var activeWorkoutVM: ActiveWorkoutViewModel
-//    @Environment(\.dismiss) private var dismiss
-//    private var exercise: Exercise {
-//        activeWorkoutVM.exercise
-//    }
-//    
-//    var body: some View {
-//        VStack(spacing: 20) {
-//            Text(exercise.name)
-//                .font(.largeTitle)
-//            
-//            ExerciseImageView(imageUrl: exercise.photo)
-//            
-//            Text(
-//                "Подходов сделано: \(activeWorkoutVM.completedSets) из \(exercise.set)"
-//            )
-//            
-//            VStack(alignment: .leading) {
-//                Text("Повторения: \(exercise.rep)")
-//                Text("Вес: \(exercise.weight)")
-//            }
-//            
-//            Text(exercise.comment)
-//            
-//            Button(activeWorkoutVM.buttonText) {
-//                withAnimation(.easeInOut(duration: 0.8)) {
-//                    activeWorkoutVM.showNextExercise()
-//                }
-//                activeWorkoutVM.finishWorkout {
-//                    dismiss()
-//                }
-//            }
-//            
-//            Spacer()
-//        }
-//        .padding()
-//    }
-//}
+//  Created by Denis Denisov on 27/6/24.
 //
-//import SwiftData
-//#Preview {
-//    let container = DataController.previewContainer
-//    let workouts = try? container.mainContext.fetch(FetchDescriptor<Workout>())
-//    let workout = workouts?.first ?? Workout.getWorkout()
-//
-//    return ActiveWorkoutView(activeWorkoutVM: ActiveWorkoutViewModel(workout: workout))
-//        .modelContainer(container)
-//}
+
+import SwiftUI
+
+struct ActiveWorkoutView: View {
+    @Bindable var activeWorkoutVM: ActiveWorkoutViewModel
+    
+    var body: some View {
+        Text("Start")
+    }
+}
+
+#Preview {
+    let container = DataController.previewContainer
+
+    return ActiveWorkoutView(activeWorkoutVM: ActiveWorkoutViewModel())
+        .modelContainer(container)
+}
--- LegoFit/ActiveWorkout/ActiveWorkoutViewModel.swift
+++ LegoFit/ActiveWorkout/ActiveWorkoutViewModel.swift
@@ -1,60 +1,13 @@
-////
-////  ActiveWorkoutViewModel.swift
-////  LegoFit
-////
-////  Created by Denis Denisov on 27/6/24.
-////
 //
-//import Observation
+//  ActiveWorkoutViewModel.swift
+//  LegoFit
 //
-//@Observable
-//final class ActiveWorkoutViewModel {
-//    
-//    var completedSets = 0
-//    
-//    var buttonText: String {
-//        if isLastExercise
-//            && completedSets == (exercise.set ?? 0) - 1 {
-//            return "Закончить"
-//        } else if completedSets == exercise.set {
-//            return "Далее"
-//        } else {
-//            return "Закончил подход"
-//        }
-//    }
-//    
-//    var exercise: Exercise {
-//        exercises[queue]
-//    }
-//    
-//    var isLastExercise: Bool {
-//        exercises.count - 1 == queue
-//    }
-//    
-//    private var queue = 0
-//    private let workout: Workout
-//    private var exercises: [ExerciseType] {
-//        workout.exercises
-//    }
-//    
-//    init(workout: Workout) {
-//        self.workout = workout
-//    }
-//    
-//    func showNextExercise() {
-//        if completedSets == exercise.set {
-//            queue += 1
-//            completedSets = 0
-//        } else {
-//            completedSets += 1
-//        }
-//    }
-//    
-//    func finishWorkout(completion: (() -> Void)?) {
-//        if isLastExercise && completedSets == exercise.set {
-//            completion?()
-//            queue = 0
-//            completedSets = 0
-//        }
-//    }
-//}
+//  Created by Denis Denisov on 27/6/24.
+//
+
+import Observation
+
+@Observable
+final class ActiveWorkoutViewModel {
+    
+}
